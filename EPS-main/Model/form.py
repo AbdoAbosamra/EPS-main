b45bb79fb4cae7c5ede143b556f9bdd2ab643898
Abdo Abo Samra <67236632+AbdoAbosamra@users.noreply.github.com>
2021-05-20 03:37:56 +0200

    Initial commit

---
+++ EPS-main/Model/form.py
@@ -0,0 +1,27 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import *
+from django import forms
+from django.forms.widgets import TextInput  , EmailInput , PasswordInput , NumberInput
+from django.forms import inlineformset_factory
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        widgets = {
+            'username': TextInput(attrs={'class': 'input100','placeholder':'User name'}),
+            'email': EmailInput(attrs={'class': 'input100','placeholder':'User name'}),
+            'first_name': TextInput(attrs={'class': 'input100','placeholder':'User name'}),
+            'last_name': TextInput(attrs={'class': 'input100','placeholder':'User name'}),
+            'password1': PasswordInput(attrs={'class': 'input100','placeholder':'User name'}),
+            'password2': PasswordInput(attrs={'class': 'input100','placeholder':'User name'}),
+            }
+
+class StudentObj(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = "__all__"
+        exclude = ['user',"Department_DS","Department_SVM","Department_KNN"]
---
+++ EPS-main/Model/urls.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.urls import path , include
+from .views import *
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('' , frist_page , name='frist_page'),
+    path('user_home/', User_Home, name='user_home'),
+    path('admin_home/', Admin_Home, name='admin_Home'),
+    path('login/', loginpage, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout1, name='logout'),
+    path('form/',Form,name='Form'),
+    path('reset_password/', auth_views.PasswordResetView.as_view() , name = 'password_reset'),
+    path('reset_password_send/', auth_views.PasswordResetDoneView.as_view() ,name = 'password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view() , name = 'password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view() , name = 'password_reset_complete'),
+
+    ################### Quiz ##############################
+
+    path('list/' ,quiz_veiw , name='quiz_veiw'),
+    path('list/<pk>/' ,quizpk_veiw , name='quizpk_veiw'),
+    path('list/<pk>/sava/' ,save_quiz_view , name='save_view'),
+    path('list/<pk>/data' ,quiz_data_view , name='quiz_data_view'),
+
+
+
+]
+
+
