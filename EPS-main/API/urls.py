28003a4c5a9e46ac7a50a8128c40a0d55eee42e4
abdo <abdel99073616@gmail.com>
2021-07-02 18:56:08 +0200

    dash

--- EPS-main/API/urls.py
+++ EPS-main/API/urls.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import(
-    ################ User ########################
-    UserListAPIVeiw,
-    UserCreateAPIVeiw,
-    UserUpdateListAPIVeiw,
-    UserRetieveAPIVeiw,
-    UserDestroyAPIView,
-)
+from .views import *
 
 urlpatterns = [
     ################ User ########################
@@ -16,4 +9,11 @@ urlpatterns = [
     path('User/<pk>' , UserRetieveAPIVeiw.as_view() ) ,
     path('User/<pk>/edit' , UserUpdateListAPIVeiw.as_view() ) ,
     path('User/<pk>/delete' , UserDestroyAPIView.as_view() ) ,
+    path('', frist_page, name='frist_page'),
+    path('user_home/', User_Home, name='user_home'),
+    path('admin_home/', Admin_Home, name='admin_Home'),
+    path('login/', loginpage, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout1, name='logout'),
+    path('form/', Form, name='Form'),
 ]
