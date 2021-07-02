c8dba779b1f1e50acdbb6f83d0d6cd5694d40f6d
Abdo Abo Samra <67236632+AbdoAbosamra@users.noreply.github.com>
2021-07-02 14:27:48 +0200

    Some additions

--- EPS-main/Model/views.py
+++ EPS-main/Model/views.py
@@ -97,20 +97,23 @@ def Form(request):
 Data = pd.read_csv('D:\EPS\Data\Data-V1.0.csv')
 X = Data.drop(["ID" , "Department" ,"IS_Chance" , "CS_Chance"], axis=1)
 y = Data["Department"]
-def Render_info(request):
+
+
+'''def Render_info(request):
     pk = request.user.id
     student = Student.objects.get(user=pk)
     df = pd.DataFrame(list(Student.objects.all().values()))
     df = df.loc[df['user_id'] == pk]
     df = df.drop(["user_id", "id", "Department_DS", "Department_SVM", "Department_KNN", "DS_acc",
                   "SVM_acc", "KNN_acc"], axis=1)
-
+    D = df.loc[: , :][0]
     form = StudentObj(instance=student)
     form = StudentObj(request.POST, instance=student)
     if form.is_valid():
         form.save()
     return request
-
+'''
+# To do Later
 
 @login_required(login_url='login')
 def DecisionTree(request):
